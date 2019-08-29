8a104f088cfd27d38514ae5a031d4c0ffe89d4eb
Perry MacMurray <mrprototypep@gmail.com>
2019-08-29 11:40:56 -0400

    Add project files.

---
+++ PixelChangeMonitor/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PixelChangeMonitor
+{
+    static class Main
+    {
+        static void Main()
+        {
+            Thread.Sleep(2000); //Wait 2 seconds before beginning monitoring process
+        }
+    }
+}
